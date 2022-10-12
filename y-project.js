f619ae04ae2844398897533152bd412475ec5061
yopox <yopoxdev@gmail.com>
2022-10-12 10:59:14 +0200

    [style] Responsive design

--- y-project.js
+++ y-project.js
@@ -9,6 +9,7 @@ export class YProject extends LitElement {
         display: flex;
         flex-flow: row;
         align-items: center;
+        font-size: var(--font_body);
       }
 
       #content {
@@ -17,23 +18,25 @@ export class YProject extends LitElement {
         padding: 0 32px;
       }
 
+
       #title {
-        font-size: 48px;
-        line-height: 48px;
+        font-size: var(--font_projects);
+        line-height: var(--font_projects);
         font-weight: 700;
       }
 
       #chips {
         display: flex;
         flex-flow: row;
-        margin: 16px 0;
+        flex-wrap: wrap;
+        margin: 8px 0 16px 0;
       }
 
       .chip {
         background-color: rgba(35, 37, 42, 0.2);
         border-radius: 16px;
         padding: 4px 8px;
-        margin-right: 8px;
+        margin: 8px 8px 0 0;
       }
 
       #promo {
@@ -65,6 +68,26 @@ export class YProject extends LitElement {
         width: 16px;
         height: 16px;
         margin-bottom: -3px;
+      }
+
+      @media screen and (max-width: 920px) {
+        #promo {
+          width: 210px;
+          height: 140px;
+        }
+      }
+
+      @media screen and (max-width: 640px) {
+        #container {
+          padding: 32px;
+          flex-flow: column;
+        }
+        
+        #promo {
+          width: 270px;
+          height: 180px;
+          margin-bottom: 16px;
+        }
       }
     `;
   }
@@ -105,7 +128,7 @@ export class YProject extends LitElement {
               .filter((s) => s !== '')
               .map((s) => {
                 let link = s.split(';');
-                return html`<a href="${link[1]}"><img class="arrow" src="assets/north_east_FILL0_wght400_GRAD0_opsz48.svg"> ${link[0]}</a>`
+                return html`<a href="${link[1]}"><img class="arrow" src="assets/north_east_FILL0_wght400_GRAD0_opsz48.svg">&nbsp;${link[0]}</a>`
               })}
           </div>
         </div>
