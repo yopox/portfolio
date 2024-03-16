bad1b6f7f351c007f44dc6b960b6bd52b8046aba
yopox <yopoxdev@gmail.com>
2024-03-16 18:15:32 +0100

    style: update the accent color

--- static/scroll.js
+++ static/scroll.js
@@ -1,8 +1,9 @@
-const accent1 = [199, 231, 207];
-const accent2 = [243, 153, 124];
+const accent1 = [243, 217, 122];
+const accent2 = [209, 241, 168];
 
 function runOnScroll() {
   let offset = window.scrollY / (document.body.offsetHeight - window.innerHeight);
+  offset = offset ** 3.5;
   const color = [
     accent1[0] + offset * (accent2[0] - accent1[0]),
     accent1[1] + offset * (accent2[1] - accent1[1]),
